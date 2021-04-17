ad9bfb8e264f0595b903f0518c2d9b42af85f772
Raju Shrestha <rshrestha9@mail.greenriver.edu>
2021-04-16 22:30:05 -0400

    added removeDups method

--- includes/functions.php
+++ includes/functions.php
@@ -1,1 +1,31 @@
 <?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+function printArr($number)
+{
+    foreach ($number as $num) {
+        echo "$num <br>";
+    }
+}
+
+function largest($number)
+{
+    $large = "";
+    foreach ($number as $num) {
+        if ($num > $large) {
+            $large = $num;
+        }
+    }
+    echo "The largest number is $large";
+}
+function removeDups($number){
+    $result = array();
+    foreach ($number as $key => $value){
+        if(!in_array($value, $result))
+            $result[$key]=$value;
+    }
+    foreach ($result as $val){
+        echo "$val <br>";
+    }
+}
