115a32edcdb515d6cd2cef59d6fb27d0ee07d656
Raju Shrestha <rshrestha9@mail.greenriver.edu>
2021-04-16 21:19:29 -0400

    added call to function largest

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+//Name: Raju Shrestha
+//04/16/2021
+//http://rshrestha9.greenriverdev.com/sdev328/pp2/
+//Turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program2</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+
+
+echo "<h1>PHP Array Practice.</h1>";
+
+$number=array(7,9,8,9,8,8,6);
+//include("includes/functions.php");
+
+//printArr($number);
+//echo "<br>";
+//largest($number);
+//echo "<br>";
+
+?>
